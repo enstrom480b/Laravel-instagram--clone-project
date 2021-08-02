0cdaaa5bdc04aad365b89e3c938de9ad02819142
Evans kiprono <ekiprono99@gmail.com>
2021-08-02 16:47:46 +0300

    Add files via upload
    welcome-email.blade.php view file that has text data

---
+++ resources/views/emails/welcome-email.blade.php
@@ -0,0 +1,7 @@
+@component('mail::message')
+This is a community of fellow developers and we love that you have joined us
+@component('mail::button', ['url' => ''])
+All the best
+Rono,<br>
+{{ config('app.name') }}
+@endcomponent
